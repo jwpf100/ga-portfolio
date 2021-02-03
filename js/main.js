5616c0beac9f1369233e4e059b83a4b01d879aff
jwpf100 <jwpf100@gmail.com>
2021-02-03 11:21:51 +0000

    Added scroll button.

--- js/main.js
+++ js/main.js
@@ -23,3 +23,26 @@ $(".image-holder").hover(function(){
 });
 
 $("button.submit").on("click",function(){alert("The functionality for submitting an email is not in place yet.") })
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  console.log(window.pageYOffset)
+  if (document.body.scrollTop > 1124 || document.documentElement.scrollTop > 1124) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
